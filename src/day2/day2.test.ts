53ddce67e02c8ea4e4e0f6a18c2dfe363c9b36d3
s10a137g <s10a137g@gmail.com>
2022-06-15 23:52:23 +0900

    Add day2-2

--- src/day2/day2.test.ts
+++ src/day2/day2.test.ts
@@ -1,5 +1,5 @@
 import { readFile } from "../index";
-import { day2_1 } from "./day2";
+import { day2_1, day2_2 } from "./day2";
 
 const testData = readFile("../data/day2/test.txt")
 const inputData = readFile("../data/day2/input.txt")
@@ -9,8 +9,8 @@ test("day2-1", (): void => {
     expect(day2_1(inputData)).toBe(1989014)
 })
 
-// test("day2-2", (): void => {
-//     console.log('day2-2 test')
-//     expect(day2_2(testData2)).toBe(5)
-//     expect(day2_2(inputData1)).toBe(1471)
-// })
+test("day2-2", (): void => {
+    console.log('day2-2 test')
+    expect(day2_2(testData)).toBe(900)
+    expect(day2_2(inputData)).toBe(2006917119)
+})
--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -27,3 +27,30 @@ export const day2_1 = (args: string[]): number => {
     // 水平方向の値 × 深さを返す
     return horizonVal * (depthDownVal - depthUpVal)
 }
+
+export const day2_2 = (args: string[]): number => {
+    // INPUTデータを行ごとに配列に変換
+    const argArray = args
+        .map(arg => arg.split(" "))
+        .filter(v => v.length === 2)
+
+
+    // aimの値を計算する必要があるため、走査中に方向を判定。
+    // 操作中にaimの値を加算する
+    let aim = 0
+    const resultArray = argArray.reduce((acc, v) => {
+        if (v[0] === "forward") {
+            return [acc[0] + parseInt(v[1]), acc[1] + (parseInt(v[1]) * aim)]
+        } else if (v[0] === "up") {
+            aim -= parseInt(v[1])
+            return [acc[0], acc[1]]
+        } else if (v[0] === "down") {
+            aim += parseInt(v[1])
+            return [acc[0], acc[1]]
+        } else {
+            return [acc[0], acc[1]]
+        }
+    }, [0, 0])
+
+    return resultArray[0] * resultArray[1]
+}
