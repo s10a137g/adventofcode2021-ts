5b91fa38ad9e705b4b144299d588342d1d1fbc17
s10a137g <s10a137g@gmail.com>
2022-06-16 08:08:05 +0900

    Refactor day2-1, day2-2

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,27 +1,25 @@
 
 export const day2_1 = (args: string[]): number => {
     // INPUTデータを行ごとに配列に変換
-    const argArray = args
-        .map(arg => arg.split(" "))
-        .filter(v => v.length === 2)
+    const argArray = args.map(arg => arg.split(" "))
 
 
     // [配列の先頭文字列ごとに、値を集計]
     const initialValue = [0, 0, 0]
     const [forwardV, downV, upV] =
-        argArray.reduce(([forwardV, downV, upV], [pos, value]) => {
+        argArray.reduce(([forwardV, downV, upV], [direction, value]) => {
             // 水平方向の値
-            if (pos === "forward") {
+            if (direction === "forward") {
                 return [forwardV + parseInt(value), downV, upV]
             }
 
             // 深さ(増加)の値
-            if (pos === "down") {
+            if (direction === "down") {
                 return [forwardV, downV + parseInt(value), upV]
             }
 
             // 深さ(増加)の値
-            if (pos === "up") {
+            if (direction === "up") {
                 return [forwardV, downV, upV + parseInt(value)]
             }
 
@@ -34,30 +32,31 @@ export const day2_1 = (args: string[]): number => {
 
 export const day2_2 = (args: string[]): number => {
     // INPUTデータを行ごとに配列に変換
-    const argArray = args
-        .map(arg => arg.split(" "))
-        .filter(v => v.length === 2)
+    const argArray = args.map(arg => arg.split(" "))
 
 
     // aimの値を計算する必要があるため、走査中に方向を判定。
     // 操作中にaimの値を加算する
     let aim = 0
     const [horizon, depth] =
-        argArray.reduce(([horizon, depth], [pos, value]) => {
+        argArray.reduce(([horizonV, depthV], [direction, value]) => {
 
-            if (pos === "forward") {
-                return [horizon + parseInt(value), depth + (parseInt(value) * aim)]
+            // 水平方向と深さ方向の進行量を計算
+            if (direction === "forward") {
+                return [horizonV + parseInt(value), depthV + (parseInt(value) * aim)]
             }
 
-            if (pos === "up") {
-                aim -= parseInt(value)
-            }
-
-            if (pos === "down") {
+            // 標的(aim)を増加
+            if (direction === "down") {
                 aim += parseInt(value)
             }
 
-            return [horizon, depth]
+            // 標的(aim)を減少
+            if (direction === "up") {
+                aim -= parseInt(value)
+            }
+
+            return [horizonV, depthV]
 
         }, [0, 0])
 
