2aca77d7ce6cac3bb594fc4ad664a1d9ec5ea440
s10a137g <s10a137g@gmail.com>
2022-06-18 11:15:35 +0900

    Move createCards to Card

--- src/day4/day4.ts
+++ src/day4/day4.ts
@@ -1,4 +1,4 @@
-import { Card } from "./Card";
+import { Card, createCards } from "./Card";
 
 export const doBingo = (cards: Card[], searchArgs: number[]): number | null => {
     const allCards = cards.concat(cards.map(card => card.transpose()))
@@ -13,45 +13,27 @@ export const doBingo = (cards: Card[], searchArgs: number[]): number | null => {
     return null
 }
 
-export const createCards = (rows: string[][]): Card[] => {
-
-    const initialValue: Card[] = []
-    let stockRow: string[][] = []
-
-    return rows.reduce((cards: Card[], row: string[]) => {
-        if (row.length === 0) {
-            cards.push(new Card(stockRow))
-            stockRow = []
-            return cards
-        } else {
-            stockRow.push(row)
-            return cards
-        }
-    }, initialValue)
-}
-
-
 export const day4_1 = (args: string[]): number => {
-        // 1行目を抽選番号として定義
-        const selectedNumbers = args[0].split(',')
+    // 1行目を抽選番号として定義
+    const selectedNumbers = args[0].split(',')
 
-        // それ以降の行数で空白行を区切りとして、Cardとして作成する
-        const carsRows = args.slice(1).map(v => v.split(/ +/).filter(v => v !== ''))
+    // それ以降の行数で空白行を区切りとして、Cardとして作成する
+    const carsRows = args.slice(1).map(v => v.split(/ +/).filter(v => v !== ''))
 
-        const cards = createCards(carsRows.slice(1))
+    const cards = createCards(carsRows.slice(1))
 
-        const currentSelectedNumbers = []
+    const currentSelectedNumbers = []
 
-        for (let n of selectedNumbers) {
-            currentSelectedNumbers.push(+n)
-            const unmarkedElements = doBingo(cards, currentSelectedNumbers)
-            if (!!unmarkedElements) {
-                return unmarkedElements * currentSelectedNumbers[currentSelectedNumbers.length - 1]
-            }
+    for (let n of selectedNumbers) {
+        currentSelectedNumbers.push(+n)
+        const unmarkedElements = doBingo(cards, currentSelectedNumbers)
+        if (!!unmarkedElements) {
+            return unmarkedElements * currentSelectedNumbers[currentSelectedNumbers.length - 1]
         }
-
-        return 0
     }
+
+    return 0
+}
 
 export const
     day4_2 = (args: string[]): number => {
