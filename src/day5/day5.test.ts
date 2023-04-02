515164193742931a7bbebf06998cbb968f451015
s10a137g <s10a137g@gmail.com>
2023-04-02 16:33:21 +0900

    Add day5

--- src/day5/day5.test.ts
+++ src/day5/day5.test.ts
@@ -1,8 +1,15 @@
 import { readFile } from '../index'
-import { day5_1 } from './day5'
+import { day5_1, day5_2 } from './day5'
 
 const testData = readFile('../data/day5/test.txt')
+const inputData = readFile('../data/day5/input.txt')
 
 test('day5-1', (): void => {
   expect(day5_1(testData)).toBe(5)
+  expect(day5_1(inputData)).toBe(5147)
+})
+
+test('day5-2', (): void => {
+  expect(day5_2(testData)).toBe(12)
+  expect(day5_2(inputData)).toBe(16925)
 })
--- src/day5/day5.ts
+++ src/day5/day5.ts
@@ -1,17 +1,44 @@
 export const day5_1 = (args: string[]): number => {
+  // ステップ1: 入力データの解析
   const lineSegmentArray = parseInput(args)
-  console.log(lineSegmentArray)
-  return 0
+  // ステップ2: 座標系の作成
+  const gridSize = 1000
+  const grid = createGrid(gridSize)
+
+  // ステップ3: 線分を座標系にマッピング
+  mapLineSegmentsToGrid(lineSegmentArray, grid)
+
+  // ステップ4: 重なる線分の数の計算結果を出力
+  return countOverlappingPoints(grid)
 }
 
-type LineSegment = {
+export const day5_2 = (args: string[]): number => {
+  // ステップ1: 入力データの解析
+  const lineSegmentArray = parseInput(args)
+  // ステップ2: 座標系の作成
+  const gridSize = 1000
+  const grid = createGrid(gridSize)
+
+  // ステップ3: 線分を座標系にマッピング(斜め線も考慮)
+  mapLineSegmentsWithDiagonalsToGrid(lineSegmentArray, grid)
+
+  // ステップ4: 重なる線分の数の計算結果を出力
+  return countOverlappingPoints(grid)
+}
+export type LineSegment = {
   x1: number
   y1: number
   x2: number
   y2: number
 }
 
-function parseInput(input: string[]): LineSegment[] {
+/**
+ * 入力データを解析する
+ *
+ * @param input 入力データ: 「x1,y1 -> x2,y2」のフォーマット
+ * @return 線分情報リスト
+ */
+export function parseInput(input: string[]): LineSegment[] {
   const lineSegments: LineSegment[] = []
 
   input
@@ -25,4 +52,116 @@ function parseInput(input: string[]): LineSegment[] {
     })
 
   return lineSegments
+}
+
+/**
+ * Grid形式の座標系を作成する
+ *
+ * @param size 座標系の大きさ
+ * @return size✕sizeの座標系
+ */
+export function createGrid(size: number): number[][] {
+  const grid: number[][] = []
+
+  for (let i = 0; i < size; i++) {
+    grid[i] = []
+
+    for (let j = 0; j < size; j++) {
+      grid[i][j] = 0
+    }
+  }
+
+  return grid
+}
+
+/**
+ * gridに座標情報をマッピングする(縦・横のみ考慮)
+ * @param lineSegments 線分リスト
+ * @param grid
+ */
+export function mapLineSegmentsToGrid(lineSegments: LineSegment[], grid: number[][]): void {
+  for (const segment of lineSegments) {
+    const { x1, y1, x2, y2 } = segment
+
+    if (x1 === x2) {
+      // 垂直線
+      const minY = Math.min(y1, y2)
+      const maxY = Math.max(y1, y2)
+
+      for (let y = minY; y <= maxY; y++) {
+        grid[x1][y]++
+      }
+    } else if (y1 === y2) {
+      // 水平線
+      const minX = Math.min(x1, x2)
+      const maxX = Math.max(x1, x2)
+
+      for (let x = minX; x <= maxX; x++) {
+        grid[x][y1]++
+      }
+    }
+  }
+}
+
+/**
+ * 重複している箇所の要素数を返す
+ * @param grid 座標系
+ */
+export function countOverlappingPoints(grid: number[][]): number {
+  let count = 0
+
+  for (const row of grid) {
+    for (const cell of row) {
+      if (cell >= 2) {
+        count++
+      }
+    }
+  }
+
+  return count
+}
+
+/**
+ * gridに座標情報をマッピングする(縦・横・斜めを考慮)
+ * @param lineSegments
+ * @param coordinateSystem
+ */
+function mapLineSegmentsWithDiagonalsToGrid(lineSegments: LineSegment[], coordinateSystem: number[][]): void {
+  lineSegments.forEach(({ x1, y1, x2, y2 }) => {
+    // 水平な線分の場合
+    if (x1 === x2) {
+      // y座標の開始位置と終了位置を決定
+      const [yStart, yEnd] = y1 < y2 ? [y1, y2] : [y2, y1]
+      // y座標の範囲内でループし、座標系の対応するセルをインクリメント
+      for (let y = yStart; y <= yEnd; y++) {
+        coordinateSystem[y][x1]++
+      }
+    }
+    // 垂直な線分の場合
+    else if (y1 === y2) {
+      // x座標の開始位置と終了位置を決定
+      const [xStart, xEnd] = x1 < x2 ? [x1, x2] : [x2, x1]
+      // x座標の範囲内でループし、座標系の対応するセルをインクリメント
+      for (let x = xStart; x <= xEnd; x++) {
+        coordinateSystem[y1][x]++
+      }
+    }
+    // 45度の斜めの線分の場合
+    else if (Math.abs(x1 - x2) === Math.abs(y1 - y2)) {
+      // x座標とy座標のステップ（増分）を決定
+      const xStep = x1 < x2 ? 1 : -1
+      const yStep = y1 < y2 ? 1 : -1
+      let x = x1
+      let y = y1
+
+      // x座標とy座標が終点に達するまでループし、座標系の対応するセルをインクリメント
+      while (x !== x2 || y !== y2) {
+        coordinateSystem[y][x]++
+        x += xStep
+        y += yStep
+      }
+      // 最後の点をインクリメント
+      coordinateSystem[y][x]++
+    }
+  })
 }
